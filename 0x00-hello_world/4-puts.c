54ca736bed7ee7e29a0832f410ec15b98ab81c76
Abongile <abongilemakaula69@gmail.com>
2023-03-19 13:15:34 -0700

    printing exact text with c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - the main function
+ * main - writing exact text with program
  *
- * Return - always 0
+ * Return: Always 0
  */
+
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
