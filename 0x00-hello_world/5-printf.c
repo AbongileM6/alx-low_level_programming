1c3b4364be22d8a5415cfe7b95d9c131613225f2
Abongile <abongilemakaula69@gmail.com>
2023-03-19 13:49:44 -0700

    using printf to program exact text with new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - programming with print
+ *
+ * Return: 0 when successsful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
