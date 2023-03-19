1e300b34901c5a21f4e027b57fa10dd904dd2049
Abongile <abongilemakaula69@gmail.com>
2023-03-19 12:42:15 -0700

    that prints exact line followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Writing a c program for printing words with characters exactly 
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{ 
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
